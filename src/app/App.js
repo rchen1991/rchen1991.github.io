c2426923f0037d2f8b0c24c3ef76062b06cf5d42
rchen1991 <rchen1991@gmail.com>
2019-06-20 01:44:37 -0700

    rearrange directory structure

--- src/app/App.js
+++ src/app/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        <Link to="/" className="logo">r|c</Link>
+        <Link to="/" className="logo">rc</Link>
         <Navbar />
       </header>
       <div>
